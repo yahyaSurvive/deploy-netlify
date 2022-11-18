1abc6c9647c33380e192e156e1c54113086bf348
yahyaSurvive <putuyahya5566@gmail.com>
2022-11-18 09:13:45 +0800

    menambahkan tailwind

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -9,14 +9,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+        <h1 className="hover:text-green-500 cursor-pointer">Hallo Yahya</h1>
       </header>
     </div>
   );
